63951ebf2ad692c0e71de6ddc71049c0999ac5dc
Callum McLuskey <callummcluskey100@gmail.com>
2023-05-19 17:29:36 +0100

    added docker script

---
+++ scripts/run.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Clear out any cached files and images from each individual rerun deployment
+docker system prune -a -f
+
+
+# run docker compose
+docker-compose build --no-cache
+docker-compose up -d 
