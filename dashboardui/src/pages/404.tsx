0fe02406ce1ac979ff23bda104299be74e7363a5
Callum McLuskey <callummcluskey100@gmail.com>
2023-05-22 18:10:00 +0100

    removed title from 404

--- dashboardui/src/pages/404.tsx
+++ dashboardui/src/pages/404.tsx
@@ -1,11 +1,6 @@
-import { Head } from "next/document";
-
 export default function Custom404() {
   return (
     <>
-      <Head>
-        <title>Uh oh</title>
-      </Head>
       <main>
         <h1>404</h1>
         <p>This page does not exist.</p>
