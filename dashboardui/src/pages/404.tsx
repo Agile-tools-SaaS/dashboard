0a1987ca961eac935ddeadf6faff2d4d46884d04
Callum McLuskey <callummcluskey100@gmail.com>
2023-05-22 17:58:15 +0100

    another nginx change (should be the last)

---
+++ dashboardui/src/pages/404.tsx
@@ -0,0 +1,15 @@
+import { Head } from "next/document";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Uh oh</title>
+      </Head>
+      <main>
+        <h1>404</h1>
+        <p>This page does not exist.</p>
+      </main>
+    </>
+  );
+}
